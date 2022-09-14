1c71969ef15262919904f11c74315de0ca76ef99
HsiaoJu Ko <hsiaojuko@HsiaoJus-iMac.local>
2022-09-14 17:02:55 +0800

    Add component - Stepper

---
+++ src/components/Stepper.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id="step-control" class="stepper">
+    <h3 class="stepper__title my-5">結帳</h3>
+    <div class="stepper__container container">
+
+      <div class="stepper__step active">
+        <div class="stepper__step__circle-container"></div>
+        <span class="stepper__step__connect-line line-one"></span>
+        <label class="stepper__step__label" for="">寄送地址</label>
+      </div>
+
+      <div class="stepper__step">
+        <div class="stepper__step__circle-container"></div>
+        <span class="stepper__step__connect-line line-two"></span>
+        <label class="stepper__step__label" for="">運送方式</label>
+      </div>
+      
+      <div class="stepper__step">
+        <div class="stepper__step__circle-container"></div>
+        <label class="stepper__step__label" for="">付款資訊</label>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Stepper',
+}
+</script>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -3,15 +3,38 @@
     <header>
       <Navbar />
     </header>
+    <main class="main-content">
+      <Stepper />
+
+      <section class="form-panel">
+        <form action="" class="form-panel__container__form">
+          <!-- DestinationForm -->
+
+          <!-- ShippingForm -->
+
+          <!-- PaymentForm -->
+          
+        </form>
+      </section>
+      
+      <!-- CartPanel -->
+      <!-- ButtonPanel -->
+
+    </main>
+    <!-- Footer -->
+
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import Stepper from '../components/Stepper.vue'
+
 export default {
   name: "Checkout",
   components: {
     Navbar,
+    Stepper,
   }
 };
 </script>
